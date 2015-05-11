dd451ebdf9b48ef7ff690d95d757b5dd11073a25
Nathan Muir <ndmuir@gmail.com>
2015-05-11 13:15:05 +1000

    Added `SessionPreferenceStore` - uses `window.sessionStorage` instead of `window.localStorage`

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: '3stack:preferences-local-storage',
-  version: '1.0.0',
+  version: '1.1.0',
   summary: 'A preference store to use with 3stack:preferences',
   git: 'https://github.com/3stack-software/meteor-preferences-local-storage',
   documentation: 'README.md'
@@ -11,6 +11,7 @@ Package.onUse(function(api){
   api.versionsFrom('METEOR@0.9.2');
 
   api.use([
+    'underscore',
     'check',
     'ejson',
     'logging'
@@ -18,7 +19,10 @@ Package.onUse(function(api){
 
   api.use('mongo', 'client', {weak: true});
 
-  api.export('LocalPreferenceStore', 'client');
+  api.export([
+    'LocalPreferenceStore',
+    'SessionPreferenceStore'
+  ], 'client');
 
   api.addFiles('preferences-local-storage.js', 'client');
 });
--- preferences-local-storage.js
+++ preferences-local-storage.js
@@ -6,43 +6,53 @@ Meteor.startup(function(){
   }
 });
 
-LocalPreferenceStore = {
-  stringify: function LocalPreferenceStore_stringify(value){
-    if (value === void 0) {
-      return 'undefined';
-    }
-    return EJSON.stringify(value);
-  },
-  parse: function LocalPreferenceStore_parse(serialized){
-    if (serialized === void 0 || serialized === "undefined") {
-      return void 0;
-    }
-    return EJSON.parse(serialized);
-  },
-  set: function LocalPreferenceStore_set(userId, name, value) {
-    var self = this;
-    window.localStorage.setItem(self.key(userId, name), self.stringify(value));
-  },
-  key: function LocalPreferenceStore_key(userId, name){
-    check(userId, userIdCheck);
-    check(name, String);
-    if (userId == null){
-      return name;
-    } else {
-      return userId + '||' + name
-    }
-  },
-  get: function LocalPreferenceStore_get(userId, name) {
-    var self = this, value;
-    try {
-      value = window.localStorage.getItem(self.key(userId, name));
-      if (value != null){
-        return this.parse(value);
-      }
-
-    } catch (e) {
-      console.error(e);
-      Log.error("LocalPreferenceStore - error getting preference \"" + name + "\" for User: \"" + userId + "\"");
-    }
+var WebStoragePreferenceStore = (function(){
+  function WebStoragePreferenceStore(store){
+    this.store = store
   }
-};
+
+  _.extend(WebStoragePreferenceStore.prototype, {
+    stringify: function WebStoragePreferenceStore_stringify(value){
+      if (value === void 0) {
+        return 'undefined';
+      }
+      return EJSON.stringify(value);
+    },
+    parse: function WebStoragePreferenceStore_parse(serialized){
+      if (serialized === void 0 || serialized === "undefined") {
+        return void 0;
+      }
+      return EJSON.parse(serialized);
+    },
+    set: function WebStoragePreferenceStore_set(userId, name, value) {
+      var self = this;
+      self.store.setItem(self.key(userId, name), self.stringify(value));
+    },
+    key: function WebStoragePreferenceStore_key(userId, name){
+      check(userId, userIdCheck);
+      check(name, String);
+      if (userId == null){
+        return name;
+      } else {
+        return userId + '||' + name
+      }
+    },
+    get: function WebStoragePreferenceStore_get(userId, name) {
+      var self = this, value;
+      try {
+        value = self.store.getItem(self.key(userId, name));
+        if (value != null){
+          return self.parse(value);
+        }
+
+      } catch (e) {
+        console.error(e);
+        Log.error("WebStoragePreferenceStore - error getting preference \"" + name + "\" for User: \"" + userId + "\"");
+      }
+    }
+  });
+  return WebStoragePreferenceStore;
+})();
+
+LocalPreferenceStore = new WebStoragePreferenceStore(window.localStorage);
+SessionPreferenceStore = new WebStoragePreferenceStore(window.sessionStorage);
